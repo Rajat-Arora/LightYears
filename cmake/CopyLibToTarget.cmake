f9d5097f606c0ce2c4f10dbf106412c9da985dc2
Rajat-Arora <rarora60_be20@thapar.edu>
2025-03-02 00:27:32 +0530

    added sfml lib to project

---
+++ cmake/CopyLibToTarget.cmake
@@ -0,0 +1,10 @@
+# Function to copy SFML DLLs to the executable directory (Windows-specific)
+function(CopyLibToTarget LIB_NAME TARGET_NAME)
+    add_custom_command(TARGET ${TARGET_NAME}
+    POST_BUILD
+    COMMAND
+    ${CMAKE_COMMAND} -E copy_if_different
+    $<TARGET_FILE:${LIB_NAME}>
+    $<TARGET_FILE_DIR:${TARGET_NAME}>
+    )
+endfunction()
---
+++ installDeps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+# Install required dependencies for sfml
+sudo apt install -y libvorbis-dev libudev-dev libflac-dev
