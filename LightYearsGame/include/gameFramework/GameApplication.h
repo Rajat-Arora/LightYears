24639eb014769b19a47a2ddd33f155b864568219
Rajat-Arora <rarora60_be20@thapar.edu>
2025-03-09 14:06:02 +0530

    Updated GameApplication to Load World and spwan an Actor

--- LightYearsGame/include/gameFramework/GameApplication.h
+++ LightYearsGame/include/gameFramework/GameApplication.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <framework/Application.h>
+#include <framework/Core.h>
 #include <memory>
 
 namespace ly
 {
+    class Actor;
     class GameApplication: public Application
     {
-
+    public:
+        GameApplication();
+        virtual void Tick(float delta_time) override;
+    
+    private:
+        float counter;
+        weak<Actor> mActorToDestroy;
     };
 
 }
--- LightYearsGame/src/gameFramework/GameApplication.cpp
+++ LightYearsGame/src/gameFramework/GameApplication.cpp
@@ -1,6 +1,37 @@
 #include "gameFramework/GameApplication.h"
+#include "framework/World.h"
+#include "framework/Actor.h"
 
 std::unique_ptr<ly::Application> GetApplication()
 {
     return std::make_unique<ly::GameApplication>();
 }
+
+namespace ly
+{
+    GameApplication::GameApplication()
+        : Application(600, 980, "Light Years", sf::Style::Titlebar | sf::Style::Close)
+    {
+        weak<World> newWorld = LoadWorld<World>();
+        newWorld.lock()->SpawnActor<Actor>();
+        mActorToDestroy = newWorld.lock()->SpawnActor<Actor>();
+        mActorToDestroy.lock()->SetTexture("/home/amr/LightYears/LightYearsGame/assets/SpaceShooterRedux/PNG/playerShip1_blue.png");
+        counter = 0;
+    }
+
+    void GameApplication::Tick(float delta_time)
+    {
+        counter += delta_time;
+        if(counter > 2.f)
+        {
+            if(!mActorToDestroy.expired())
+            {
+                mActorToDestroy.lock()->Destroy();
+            }
+        }
+
+    }
+
+    
+} // namespace ly
+
